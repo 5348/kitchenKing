13769aae866cd38fe8caf5cc1107862f8cce6072
harshit <harshitmishra5348@gmail.com>
2019-07-01 01:50:12 +0530

    first commit for dish project

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component{
+
+    renderComments(dish) {
+      
+}
+
+    render() {
+        const dish = this.props.selectedDish;
+
+        if (dish != null)
+            return(
+                <div className = 'row'>
+                    <div className = 'col-12 col-md-5 m-1'>
+                        <Card>
+                            <CardImg top src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className = 'col-12 col-md-5 m-1'>
+                        {this.renderComments(this.props.selectedDish)}
+                    </div>
+                </div>
+            );
+        else
+            return(<div></div>);
+    }
+}
+
+export default DishDetail;
