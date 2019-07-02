d33d16a4a6fe55d52ba9c20c618d237e29c464fb
161b292 <harshitmishra5348@gmail.com>
2019-07-03 02:29:52 +0530

    presentation and container components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
-import { Navbar , NavbarBrand }  from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 import './App.css';
-import {DISHES} from './shared/dishes';
+
 
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
-
+// since no stated therefore no constructor therefore no props will be ther
+  
 
   
-  render(){
-   return (
-  <div>
-    <Navbar dark color="primary">
-    <div className="container">
-    <NavbarBrand href="/">victoria falls</NavbarBrand>
+render() {
+  return (
+    <div className="App">
+      <Main />
     </div>
-    </Navbar>
-    <Menu dishes={this.state.dishes} />
-  </div>
-);
+  );
 }
 }
 
--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,13 +3,58 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component{
 
+  componentDidMount(){
+      console.log("Dish Detail component  mount is invoked");
+  }
+  componentDidUpdate(){
+    console.log("Dish Detail component  update is invoked");
+}
+
+
+
+
+
+
+
     renderComments(dish) {
-      
+        if(dish != null) { 
+            const dishComments = dish.comments.map((comment) => {    
+                return (
+                    
+                    <div>
+                        <li>{comment.comment}</li>
+                        <li>-- {comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+                    </div>
+                    
+                );
+              });              
+            return(
+                <div>
+                    <Card>
+                        <CardTitle><h4>Comments</h4></CardTitle>
+                        <CardBody>
+                            <ul className = 'list-unstyled'>
+                                {dishComments}
+                            </ul>
+                        </CardBody>
+                    </Card>
+                </div>
+            )
+        }
+        else { 
+            return(
+                null
+            )
+        }
 }
 
     render() {
-        const dish = this.props.selectedDish;
-
+        console.log("Dish Detail component  render is invoked");
+        // console.log("1st check point");
+        const dish = this.props.dish;
+        console.log(dish);
+        // console.log("this is the 2nd check point");
+  
         if (dish != null)
             return(
                 <div className = 'row'>
@@ -23,7 +68,7 @@ class DishDetail extends Component{
                         </Card>
                     </div>
                     <div className = 'col-12 col-md-5 m-1'>
-                        {this.renderComments(this.props.selectedDish)}
+                        {this.renderComments(this.props.dish)}
                     </div>
                 </div>
             );
